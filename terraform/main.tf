a21cb1fc95946b77b773666da76726f80e6f79b3
Glenn <gcrosby15@gmail.com>
2024-04-19 10:06:37 -0500

    update provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 terraform {
+
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 
-  required_version = "~> 1.2"
+  required_version = "~> 1.7"
 
   backend "s3" {
     bucket = "online-resume-backend-tf-state"
