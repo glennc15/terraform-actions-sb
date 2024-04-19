611b2af5c7b3bb11093a29056013e0afd3b0e621
Glenn <gcrosby15@gmail.com>
2024-04-18 19:50:23 -0500

    add terraform infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+   backend "s3" {
+       bucket = "online-resume-backend-tf-state"
+       key    = "/terraform-actions-sb/terraform-state"
+       region = "east-us-1"
+   }
+
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "my-website-87654"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::my-website-87654/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
+}
